717506a6dbfba0f103df64e34483f302f03d9c5e
HarpeBlue <hspe2507@gmail.com>
2022-09-09 16:34:24 -0500

    useState con estado compuesto

--- src/components/UseState.js
+++ src/components/UseState.js
@@ -3,36 +3,42 @@ import React, { useEffect, useState } from "react";
 const SECURITY_CODE = "paradigma";
 
 export const UseState = ({ name }) => {
-  const [value, setValue] = useState("");
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [state, setState] = useState({
+    error: false,
+    loading: false,
+    value: "",
+  });
 
   useEffect(() => {
-    if (!!loading)
+    if (!!state.loading)
       setTimeout(() => {
-        if (value !== SECURITY_CODE) {
-          setError(true);
+        if (state.value !== SECURITY_CODE) {
+          setState((prev) => ({ ...prev, error: true }));
         } else {
-          setError(false);
+          setState((prev) => ({ ...prev, error: false }));
         }
-        setLoading(false);
+        setState((prev) => ({ ...prev, loading: false }));
       }, 5000);
-  }, [loading]);
+  }, [state.loading]);
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el codigo de seguridad.</p>
-      {error && !loading && <p>"Error: el codigo es incorrecto"</p>}
-      {loading && <p>"Cargando..."</p>}
+      {state.error && !state.loading && <p>"Error: el codigo es incorrecto"</p>}
+      {state.loading && <p>"Cargando..."</p>}
 
       <input
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
+        value={state.value}
+        onChange={(event) =>
+          setState((prev) => ({ ...prev, value: event.target.value }))
+        }
         placeholder="Codigo de seguridad"
         type="text"
       />
-      <button onClick={() => setLoading((prev) => !prev)}>Comprobar</button>
+      <button onClick={() => setState((prev) => ({ ...prev, loading: true }))}>
+        Comprobar
+      </button>
     </div>
   );
 };
